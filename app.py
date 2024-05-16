e578c2a9c17c7e0f1a00ff2986541025c08856e7
vishujaiswal123as <154772257+vishujaiswal123as@users.noreply.github.com>
2024-05-16 15:39:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-@st.experimental_singleton
+@st.experimental_memo
 def get_driver():
     return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
